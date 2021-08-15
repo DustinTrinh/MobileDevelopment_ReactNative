356f877a42dc0344fcbf2e012b576c12ab4cacf1
Dustin Trinh <dustintrinh98@gmail.com>
2021-08-14 22:12:23 -0400

    Test Push First project

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const ComponentsScreen = () =>{
+    return <Text>Just Test Screen :D </Text>;
+}
+
+const styles = StyleSheet.create({
+    textStyle:{
+        fontSize: 30
+    }
+})
